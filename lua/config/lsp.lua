906c048db759a5aef8921bfd4e04276da1bcc9fd
lverbru <lverbru@lverbru.home.arpa>
2025-09-25 14:32:50 +0200

    Enabling C lsp and disabling the others

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -124,14 +124,14 @@ vim.lsp.config("*", {
 
 -- A mapping from lsp server name to the executable name
 local enabled_lsp_servers = {
-  pyright = "delance-langserver",
-  ruff = "ruff",
-  lua_ls = "lua-language-server",
+  -- pyright = "delance-langserver",
+  -- ruff = "ruff",
+  -- lua_ls = "lua-language-server",
   -- ltex = "ltex-ls",
   -- clangd = "clangd",
-  vimls = "vim-language-server",
-  bashls = "bash-language-server",
-  yamlls = "yaml-language-server",
+  -- vimls = "vim-language-server",
+  -- bashls = "bash-language-server",
+  -- yamlls = "yaml-language-server",
 }
 
 for server_name, lsp_executable in pairs(enabled_lsp_servers) do
