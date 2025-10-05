541fdff3213c37e691f69417ded610b9eea1ed2a
theengineerone <verbruggenlucas2@gmail.com>
2025-10-05 20:07:26 +0200

    Adding debugger to lsps

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -142,13 +142,15 @@ vim.api.nvim_create_autocmd("FileType", {
     local root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" ,".project"})
     -- Use root_dir as workspace
     local workspace_dir = root_dir
+    local mason_path = vim.fn.stdpath("data") .. "/mason/packages/java-debug-adapter/extension/server/"
+    local bundles = vim.fn.glob(mason_path .. "com.microsoft.java.debug.plugin-*.jar", true)
 
     local config = {
       cmd = { "jdtls" },
       root_dir = require("jdtls.setup").find_root({ ".git", "pom.xml", "build.gradle" }),
       capabilities = require("lsp_utils").get_default_capabilities(),
       settings = { java = { configuration = { updateBuildConfiguration = "interactive" } } },
-      init_options = { bundles = {} },
+      init_options = { bundles = bundles },
     }
 
     jdtls.start_or_attach(config)
